9eb601dbaa33eae34c3f1375320095421629d53b
Arthur Raposo <theraposofox@gmail.com>
2023-02-23 18:00:52 -0300

    initial commit

--- react-ga-test/src/App.js
+++ react-ga-test/src/App.js
@@ -6,14 +6,10 @@ const TRACKING_ID = "G-TG2HM26PD9"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 function App() {
 
-    function sayHello() {
-        alert('Hello!');
-    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <button onClick={sayHello}>Testar Google Analytics</button>
           <Contact></Contact>
       </header>
     </div>
--- react-ga-test/src/useAnalyticsEventTracker.jsx
+++ react-ga-test/src/useAnalyticsEventTracker.jsx
@@ -3,9 +3,12 @@ import ReactGA from "react-ga";
 
 const useAnalyticsEventTracker = (category="Blog category") => {
     const eventTracker = (action = "test action", label = "test label") => {
+        ReactGA.event({
+            category: 'Editing',
+            action: 'Deleted Component',
+            label: 'Game Widget'
+        });
         ReactGA.event({category, action, label});
-        console.log("ok")
-        console.log(ReactGA.event({category, action, label}))
     }
     return eventTracker;
 }
